e24dbece01830244def93925ebf76d227ce14ca3
Andrew <johnsonkids.andrew@gmail.com>
2018-04-02 19:02:40 -0600

    Just the initial dialog design. Feel free to tweak it

--- DummyInstaller/MainWindow.xaml.cs
+++ DummyInstaller/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace DummyInstaller
 {
@@ -23,6 +26,56 @@ namespace DummyInstaller
         public MainWindow()
         {
             InitializeComponent();
+            
+            try
+            {
+                String[] steamGames = Directory.GetDirectories(@"C:\Program Files (x86)\Steam\steamapps\common");
+                for (int i = 0; i < steamGames.Length; i++)
+                {
+                    SteamGamesListBox.Items.Add(new SteamGameItem(steamGames[i]));
+                }
+            }
+            catch
+            {
+                SteamGamesListBox.Items.Add("No Steam Games Found");
+            }
+            SteamGamesListBox.Items.Add(new SteamGameItem());
+            
+        }
+
+        private void SteamGamesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SteamGameItem selectedItem = (SteamGameItem)e.AddedItems[0];
+            string path = "";
+            if (selectedItem.isDefault)
+            {
+                var dlg = new CommonOpenFileDialog();
+                dlg.Title = "My Title";
+                dlg.IsFolderPicker = true;
+                
+
+                dlg.AddToMostRecentlyUsedList = false;
+                dlg.AllowNonFileSystemItems = false;
+                
+                dlg.EnsureFileExists = true;
+                dlg.EnsurePathExists = true;
+                dlg.EnsureReadOnly = false;
+                dlg.EnsureValidNames = true;
+                dlg.Multiselect = false;
+                dlg.ShowPlacesList = true;
+
+                if (dlg.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    path = dlg.FileName;
+                    // Do something with selected folder string
+                }
+            }
+            else
+            {
+                path = selectedItem.gamePath;
+            }
+            FileLocationTextBox.Text = path;
+            installButton.IsEnabled = true;
         }
     }
 }
---
+++ DummyInstaller/SteamGameItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class SteamGameItem
+{
+    public String gamePath;
+    public bool isDefault = false;
+	public SteamGameItem(String gamePath)
+	{
+        this.gamePath = gamePath;
+	}
+
+    public SteamGameItem()
+    {
+        gamePath = "\\My Game is not listed";
+        isDefault = true;
+    }
+
+    public override string ToString()
+    {
+        return gamePath.Split('\\')[gamePath.Split('\\').Length -1];
+    }
+}
